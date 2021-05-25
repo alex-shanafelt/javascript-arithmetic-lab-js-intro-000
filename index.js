d988afd1e0d26c40d74f26fb14ea559b98d63faa
Alex Shanafelt <alexpshanafelt@gmail.com>
2021-05-25 18:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function add() {
-  var a;
-  var b;
+  var a
+  var b
 
   return a+b
 }
 
 function subtract() {
-  var a;
-  var b;
+  var a
+  var b
 
   return a-b
 }
 
 function multiply() {
-  var a;
-  var b;
+  var a
+  var b
 
   return a*b
 }
 
 function divide() {
-  var a;
-  var b;
+  var a
+  var b
 
   return a/b
 }
